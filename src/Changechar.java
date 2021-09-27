162900c57cd1ccf558f62e20d0978a67799086b5
H-javier <67370502+H-javier@users.noreply.github.com>
2021-09-27 20:43:43 +0800

    first add

---
+++ src/Changechar.java
@@ -0,0 +1,12 @@
+public class Changechar {
+    public Changechar() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("北京\t上海\t广东");
+        System.out.println("Javier\nYYDS");
+        System.out.println("C:\\java_projects");
+        System.out.println("Jav:\"空进去\"");
+        System.out.println("javier\rjav");
+    }
+}
