245e4a3349b4b0b32e9e2dbdd769b7a1a7831ab6
H-javier <67370502+H-javier@users.noreply.github.com>
2021-09-28 22:23:22 +0800

    Different data type test

---
+++ src/Var_2.java
@@ -0,0 +1,15 @@
+public class Var_2 {
+    public static void main(String[] args) {
+        //变量的定义
+        int age = 21;
+        double score = 88.8;
+        char gender = '男';
+        String name = "Javier";
+        //输出信息
+        System.out.println("message:");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(gender);
+    }
+}
