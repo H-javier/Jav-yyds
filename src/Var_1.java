adb3206325aa756c83b1989665dd8eb5b94ec0ba
H-javier <67370502+H-javier@users.noreply.github.com>
2021-09-28 21:55:09 +0800

    The test of var

--- src/Var_1.java
+++ src/Var_1.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Var_1 {
+public class Var_1 {
+    public static void main(String[] args) {
+        //声明变量
+        int a;
+        a = 100;
+        System.out.println(a);
+
+        int b = 800;
+        System.out.println(b);
+    }
 }
