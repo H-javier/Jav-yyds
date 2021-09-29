d46629ea94aabb42bfde7a2c5269cf9c198c37d0
H-javier <67370502+H-javier@users.noreply.github.com>
2021-09-29 21:59:39 +0800

    The usage of plus

---
+++ src/Plus.java
@@ -0,0 +1,7 @@
+public class Plus {
+    public static void main(String[] args) {
+        System.out.println(666 + 888);
+        System.out.println("666" + 888);
+        System.out.println(666 + 888 + "Jav");
+    }
+}
